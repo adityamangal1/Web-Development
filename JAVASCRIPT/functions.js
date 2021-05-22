fa96501791f1a0c8a4f9445434f51c8de1853964
aditya mangal <adityamangal0202@gmail.com>
2021-05-22 17:24:36 +0530

    JS Files

---
+++ JAVASCRIPT/functions.js
@@ -0,0 +1,13 @@
+console.log("hi");
+let a = 5;
+let name = "Aditya";
+console.log(a + " " + name + " " + "ohk");
+
+let name1 = "bye";
+
+function PRintnama(name) {
+  console.log(name);
+}
+
+PRintnama("aditya mangal");
+PRintnama(name1);
